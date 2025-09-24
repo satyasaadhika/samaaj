54bd3f669f732fe975dea06ac3fa4482f5eb52a4
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-09-24 22:06:22 +0530

    add responsive sidebar with slide transition

--- samaaj-client/src/components/Sidebar.jsx
+++ samaaj-client/src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import MenuItems from './Menuitems';
 
-const Sidebar = () => {
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
+
+    const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>This is a sidebar</h1>
+    <div className={ `w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
+      <div className='w-full'>
+           <img onClick={() => navigate('/')} src={assets.logo} alt="logo" className='w-26 ml-7 my-2 cursor-pointer'/>
+            <hr className='border-gray-300 mb-8' />
+            {/* Menu Items */}
+
+            <MenuItems setSidebarOpen={setSidebarOpen} />
+
+      </div>
     </div>
   )
 }
