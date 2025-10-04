ff7350e7f121e063957e0e5739a409ff9cac7e73
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-10-04 22:13:26 +0530

    add conditional rendering for StoryViewer component

--- samaaj-client/src/components/StoriesBar.jsx
+++ samaaj-client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData } from '../assets/assets';
 import { Plus } from 'lucide-react';
 import moment from 'moment';
 import StoryModal from './StoryModal';
+import StoryViewer from './StoryViewer';
 
 const StoriesBar = () => {
 
@@ -35,7 +36,7 @@ const StoriesBar = () => {
         {/* Story Card */}
         {
           stories.map((story, index)=>(
-            <div key={index} className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
+            <div onClick={()=> setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}>
                 <img src={story.user.profile_picture} alt='story' className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow'/>
                 <p className='absolute top-18 left-3 text-white/60 text-sm truncate max-w-24'>{story.content}</p>
                 <p className='text-white absolute bottom-1 right-2 z-10 text-xs'>{moment(story.createdAt).fromNow()}</p>
@@ -57,6 +58,8 @@ const StoriesBar = () => {
       </div>
         {/* Add Story Model*/}
         {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories}/>}
+        {/* View Story Model*/}
+        {viewStory && <StoryViewer viewStory= {viewStory} setViewStory={setViewStory}/>}
 
     </div>
   )
