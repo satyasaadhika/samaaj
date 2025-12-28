a8a9c1b9e1e35279829c7bfe3f6ee9147d20f4ab
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-12-28 19:51:50 +0530

    add PostCard component for rendering posts

--- samaaj-client/src/components/PostCard.jsx
+++ samaaj-client/src/components/PostCard.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
+import moment from 'moment'
+import { dummyUserData } from '../assets/assets';
 
-const PostCard = () => {
+const PostCard = ({post}) => {
+
+  const postWithHashtags = post.content.replace(/(#\w+)/g, '<span class="text-indigo-600 cursor-pointer">$1</span>');
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentUser = dummyUserData; // Replace with actual current user data
+  const handleLike = async() => {
+    if(likes.includes(currentUser._id)){
+      setLikes(likes.filter(id => id !== currentUser._id));
+    } else {
+      setLikes([...likes, currentUser._id]);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
+      {/*User Info*/}
+      <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <img src={post.user.profile_picture} alt='profile picture'className='w-10 h-10 rounded-full shadow'/>
+        <div>
+          <div className='flex items-center space-x-1'>
+              <span>{post.user.full_name}</span>
+              <BadgeCheck className='w-4 h-4 text-blue-500'/>
+          </div>
+          <div className='text-gray-500 text-sm'>@{post.user.username}.{moment(post.createdAt).fromNow()} </div>
+        </div>
+      </div>  
+      {/*Post Content*/}
+      {post.content && <div className='text-gray-800 text-sm whitespace-pre-line 'dangerouslySetInnerHTML={{__html: postWithHashtags}}/>}
+
+      {/*Post Image*/}
+      <div className='grid grid-cols-2 gap-2'>
+        {post.image_urls.map((img, index) => (
+          <img key={index} src={img} alt="" className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto '}`}/>
+        ))}
+      </div>
+
+      {/*Post Actions*/}
+      <div className='flex item-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300'>
+        <div className='flex items-center gap-1'>
+          {/* Like Button */}
+          <Heart className={`w-4 h-4 cursor-pointer ${likes.includes(currentUser._id) && 'text-red-600 fill-red-600'}`} onClick={handleLike}/>
+          <span>{likes.length}</span>
+        </div>
+        {/* Comment Count */}
+        <div className='flex items-center gap-1'>
+          <MessageCircle className='w-4 h-4'/>
+          <span>{12}</span>
+        </div>
+        {/* Share Count */}
+        <div className='flex items-center gap-1'>
+          <Share2 className='w-4 h-4'/>
+          <span>{7}</span>
+        </div>
+
+      </div>
+
     </div>
   )
 }
 
-export default PostCard
+export default PostCard;
