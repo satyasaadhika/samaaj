99e687a67699e6cdb6e4c426f0353c99f5176f04
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-12-28 20:20:57 +0530

    add recent message UI with timestamp and unread badge

--- samaaj-client/src/components/RecentMessages.jsx
+++ samaaj-client/src/components/RecentMessages.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { dummyRecentMessagesData } from '../assets/assets';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import moment from 'moment';
 
 const RecentMessages = () => {
 
@@ -23,8 +24,18 @@ const RecentMessages = () => {
       <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
         {
           messages.map((message, index)=> (
-            <Link key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
+            <Link to={`/messages/${message.from_user_id.id}`} key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
                 <img src={message.from_user_id.profile_picture} alt="" className='w-8 h-8 rounded-full'/>
+                <div className='w-full'>
+                  <div className='flex justify-between'>
+                    <p className='font-medium'>{message.from_user_id.full_name}</p>
+                    <p className='text-[10px] text-slate-400'>{moment(message.createAt).fromNow()}</p>
+                  </div>
+                  <div className='flex justify-between'>
+                    <p className='text-gray-500'>{message.text ? message.text : 'Media'}</p>
+                    {!message.seen && <p className='bg-indigo-500 text-white w-4 h-4 flex items-center justify-center rounded-full text-[10px]'>1</p>}
+                  </div>
+                </div>
             </Link>
           ))
         }
