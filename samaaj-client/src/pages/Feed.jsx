808291f8f1344b113c058382059359ef6da0e58c
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-12-28 19:53:34 +0530

    Add post list and sponsored section

--- samaaj-client/src/pages/Feed.jsx
+++ samaaj-client/src/pages/Feed.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
-import { dummyPostsData } from '../assets/assets';
+import { assets, dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
+import PostCard from '../components/PostCard';
+import RecentMessages from '../components/RecentMessages';
 
 const Feed = () => {
 
@@ -24,15 +26,21 @@ const Feed = () => {
       <div className=''>
          <StoriesBar />
          <div className='p-4 space-y-6'>
-          List of posts here
+          {feeds .map((post)=> (
+            <PostCard key={post._id} post={post}/>
+          ))}
          </div>
       </div>
       {/* Right Sidebar */}
-      <div>
-          <div>
-            <h1>Sponsored</h1>
+      <div className='max-xl:hidden sticky top-0'>
+          <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+            <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+            <img src={assets.sponsored_img} alt='' className='w-75 h-50 rounded-md'/>
+            <p className='text-slate-600'>Email Marketing</p>
+            <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
           </div>
-          <h1>Recent Messages</h1>
+
+          <RecentMessages />
       </div>
     </div>
   ) : <Loading />;
