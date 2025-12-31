08fffc506fab08995c73edcac1dd0108d6f956cf
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2025-12-31 19:26:25 +0530

    add UI layout and search input section

--- samaaj-client/src/pages/Discover.jsx
+++ samaaj-client/src/pages/Discover.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { dummyConnectionsData } from '../assets/assets';
+import { Search } from 'lucide-react';
 
 const Discover = () => {
 
@@ -18,11 +19,32 @@ const Discover = () => {
     }
   }
 
-
-
   return (
-    <div>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+         {/* Title */}
+        <div className='mb-8'>
+          <h2 className='text-3xl font-bold text-slate-900 mb-2'>Discover People</h2>
+          <p className='text-slate-600'>Find and connect with people in your community</p>
+        </div>
 
+        {/* Search Bar */}
+        <div className='mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80'>
+          <div className='p-6'>
+            <div className='relative'>
+              <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5'/>
+              <input
+                type="text"
+                placeholder='Search for people...'
+                className='pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm'
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyUp={handleSearch}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
