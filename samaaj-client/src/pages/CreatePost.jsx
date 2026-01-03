e9466db03f73810f2527ab48c90709479f03c3be
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2026-01-03 20:56:18 +0530

    design create post page with user info, textarea, and image gallery

--- samaaj-client/src/pages/CreatePost.jsx
+++ samaaj-client/src/pages/CreatePost.jsx
@@ -1,8 +1,68 @@
 import React from 'react'
+import { useState } from 'react'
+import { dummyUserData } from '../assets/assets'
+import { Image, X } from 'lucide-react';
+
 
 const CreatePost = () => {
+
+  const [content, setContent] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const user = dummyUserData;
+
   return (
-    <div>CreatePost</div>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+        {/* Title Section */}
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Create Post</h1>
+          <p className='text-slate-600'>Share your thoughts with the community</p>
+        </div>
+        {/* Post Creation Form */}
+        <div className='max-w-xl bg-white p-4 sm:p-8 sm:pb-3 rounded-xl shadow-md space-y-4'>
+          {/* Header */}
+          <div className='flex items-center gap-3'>
+            <img src={user.profile_picture} alt="User Profile" className='w-12 h-12 rounded-full shadow'/>
+            <div>
+              <h2 className='font-semibold'>{user.full_name}</h2>
+              <p className='text-sm text-gray-500'>{user.username}</p>
+            </div>
+          </div>
+
+          {/* Textarea */}
+          <textarea className='w-full resize-none max-h-20 mt-4 text-sm outline-none placeholder-gray-400' placeholder='What is on your mind?' onChange={(e)=>setContent(e.target.value)} value={content}/>
+
+          {/* Image Upload */}
+          {
+            images.length > 0 && <div className='flex flex-wrap gap-2 mt-4'>
+              {images.map((image, i) => (
+                <div key={i} className='relative group'>
+                  <img src={URL.createObjectURL(image)} alt="Uploaded Image" className='h-20 rounded-md'/>
+                  <div onClick={()=>setImages(images.filter((_, index) => index !== i))} className='absolute hidden group-hover:flex justify-center items-center top-0 right-0 left-0 bg-black/40 rounded-md cursor-pointer'>
+                    <X className='w-6 h-6 text-white' />
+                  </div>
+                </div>
+              ))}
+            </div>
+          }
+
+          {/* Image Input and Submit Button */}
+          <div className='flex items-center justify-between pt-3 border-t border-gray-300'>
+            <label htmlFor='images' className='flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 transition cursor-pointer'>
+              <Image className='size-6'/>
+            </label>
+
+            <input type="file" id='images' className='hidden' accept='image/*' multiple onChange={(e)=>setImages([...images, ...e.target.files])}/>
+
+            <button className='text-sm bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white font-medium px-8 py-2 rounded-md cursor-pointer'>
+              Publish Post
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
