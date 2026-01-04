f260de252087eac9964dbd2b5a1bc8164c1ca904
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2026-01-04 20:43:32 +0530

    create ProfileModal for editing user details with profile picture preview

---
+++ samaaj-client/src/components/ProfileModal.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { dummyUserData } from '../assets/assets'
+import { Pencil } from 'lucide-react';
+
+const ProfileModal = () => {
+
+  const user = dummyUserData;
+  const [editForm, setEditForm] = React.useState({
+    username: user.username,
+    bio: user.bio,
+    location: user.location,
+    profile_picture: user.null,
+    full_name: user.full_name,
+  });
+
+  const handleSaveProfile = async(e) => {
+    // Logic to save profile changes
+    e.preventDefault();
+  }
+  return (
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-110 h-screen overflow-y-scroll bg-black/50'>
+      <div className='max-w-2xl sm:py-6 mx-auto'>
+        <div className='bg-white rounded-lg shadow p-6'>
+          <h1 className='text-2xl font-bold text-gray-900 mb-6'>Edit Profile</h1>
+
+          <form className='space-y-4' onSubmit={handleSaveProfile}>
+            {/* Profile Picture Upload */}
+            <div className='flex flex-col items-start gap-3'>
+              <label htmlFor='profile_picture' className='block text-sm font-medium text-gray-700 mb-1'>
+                Profile Picture
+                <input hidden type="file" accept='image/*' id='profile_picture' className='w-full p-3 border border-gray-200 rounded-lg'
+                onChange={(e) => setEditForm({...editForm, profile_picture: e.target.files[0]})}/>
+                <div className='group/profile relative'>
+                  <img src={editForm.profile_picture ? URL.createObjectURL(editForm.profile_picture) : user.profile_picture} alt='Profile' className='w-24 h-24 rounded-full object-cover mt-2'/>
+                  <div className='absolute hidden group-hover/profile:flex top-0 left-0 right-0 bottom-0 bg-black/20 rounded-full items-center justify-center'>
+                    <Pencil className='w-5 h-5 text-white'/>
+                  </div>
+                </div>
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileModal
