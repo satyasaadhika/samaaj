8bc6141d446b74fe6d609f2de06dc05519f37254
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2026-01-05 19:54:04 +0530

    add cover photo upload and editable user details

--- samaaj-client/src/components/ProfileModal.jsx
+++ samaaj-client/src/components/ProfileModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { dummyUserData } from '../assets/assets'
 import { Pencil } from 'lucide-react';
 
-const ProfileModal = () => {
+const ProfileModal = ({setShowEdit}) => {
 
   const user = dummyUserData;
   const [editForm, setEditForm] = React.useState({
@@ -10,6 +10,7 @@ const ProfileModal = () => {
     bio: user.bio,
     location: user.location,
     profile_picture: user.null,
+    cover_photo: user.null,
     full_name: user.full_name,
   });
 
@@ -38,6 +39,60 @@ const ProfileModal = () => {
                 </div>
               </label>
             </div>
+
+            {/* Cover Photo Upload */}
+            <div className='flex flex-col items-start gap-3'>
+              <label htmlFor='cover_photo' className='block text-sm font-medium text-gray-700 mb-1'>
+                Cover Photo
+                <input hidden type="file" accept='image/*' id='cover_photo' className='w-full p-3 border border-gray-200 rounded-lg'
+                onChange={(e) => setEditForm({...editForm, cover_photo: e.target.files[0]})}/>
+                <div className='group/cover relative'>
+                  <img src={editForm.cover_photo ? URL.createObjectURL(editForm.cover_photo) : user.cover_photo} alt="Cover" className='w-80 h-40 rounded-lg bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 object-cover mt-2'/>
+                  <div className='absolute hidden group-hover/cover:flex top-0 left-0 right-0 bottom-0 bg-black/20 rounded-lg items-center justify-center'>
+                    <Pencil className='w-5 h-5 text-white'/>
+                  </div>
+                </div>
+              </label>
+            </div>
+
+            {/* Full Name */}
+            <div>
+              <label htmlFor='full_name' className='block text-sm font-medium text-gray-700 mb-1'>
+                Full Name
+              </label>
+              <input type="text" id='full_name' value={editForm.full_name} onChange={(e) => setEditForm({...editForm, full_name: e.target.value})} className='w-full p-3 border border-gray-200 rounded-lg' placeholder='Please enter your full name'/>
+            </div>
+            {/* Username */}
+            <div>
+              <label htmlFor='username' className='block text-sm font-medium text-gray-700 mb-1'>
+                Username    
+              </label>
+              <input type="text" id='username' value={editForm.username} onChange={(e) => setEditForm({...editForm, username: e.target.value})} className='w-full p-3 border border-gray-200 rounded-lg' placeholder='Please enter your username'/>
+            </div>
+            {/* Bio */}
+            <div>
+              <label htmlFor='bio' className='block text-sm font-medium text-gray-700 mb-1'>
+                Bio
+              </label>
+              <textarea rows={3} id='bio' value={editForm.bio} onChange={(e) => setEditForm({...editForm, bio: e.target.value})} className='w-full p-3 border border-gray-200 rounded-lg' placeholder='Please enter your bio'/>
+            </div>
+            {/* Location */}
+            <div>
+              <label htmlFor='location' className='block text-sm font-medium text-gray-700 mb-1'>
+                Location
+              </label>
+              <input type="text" id='location' value={editForm.location} onChange={(e) => setEditForm({...editForm, location: e.target.value})} className='w-full p-3 border border-gray-200 rounded-lg' placeholder='Please enter your location'/>
+            </div>
+            {/* Save Button */}
+            <div className='flex justify-end space-x-3 pt-6'>
+              <button type='button' className='px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-colors cursor-pointer' onClick={() => setShowEdit(false)}>
+                Cancel
+              </button>
+              <button type='submit' className='px-4 py-2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer'>
+                Save Changes
+              </button>
+            </div>
+
           </form>
         </div>
       </div>
