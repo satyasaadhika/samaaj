54a4a70dcecb52e1d15d1b7279b019df33133ed9
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2026-05-18 14:21:19 +0530

    Fixed error

--- samaaj-server/server.js
+++ samaaj-server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Samaaj Server is running');
 });
-app.use('./api/inngest', serve({ client: inngest, functions }));
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
